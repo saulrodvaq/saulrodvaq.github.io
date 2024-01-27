4eb7d54309f1408c42b0ed2083a0193d85a0ed82
maro <saulrodvaq@gmail.com>
2024-01-26 19:08:08 -0600

    a

--- login.js
+++ login.js
@@ -1,19 +1,19 @@
 document.getElementById('loginForm').addEventListener('submit', function (event) {
   event.preventDefault(); 
 
-  var username = document.getElementById('email').value;
+  var email = document.getElementById('email').value;
   var password = document.getElementById('password').value;
 
   var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
   var passwordRegex = /^(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\/\-])(?=.*[A-Z])(?=.*\d.*\d).{8,}$/;
 
-  var isValidEmail = emailRegex.test(username);
+  var isValidEmail = emailRegex.test(email);
   var isValidPassword = passwordRegex.test(password);
 
   if (isValidEmail && isValidPassword) {
     alert('Login exitoso');
-    console.log("Username: " + username, "Password: " + password);
+    console.log("Email: " + email, "Password: " + password);
     window.location.href = 'index.html';
   } else {
     var errorMessage = '';
