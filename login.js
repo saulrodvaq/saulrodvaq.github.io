facceff6712185364eae3954987b520a28591040
maro <saulrodvaq@gmail.com>
2024-01-26 19:23:39 -0600

    aadada

--- login.js
+++ login.js
@@ -14,6 +14,7 @@ document.getElementById('loginForm').addEventListener('submit', function (event)
   if (isValidEmail && isValidPassword) {
     alert('Login exitoso');
     console.log("Email: " + email, "Password: " + password);
+    location.reload();
   } else {
     var errorMessage = '';
 
