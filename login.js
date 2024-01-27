7dae70e9ed4f05ccf55676acbb1d8586cde2f991
maro <saulrodvaq@gmail.com>
2024-01-26 20:16:11 -0600

    jj

--- login.js
+++ login.js
@@ -1,31 +1,33 @@
-document.getElementById('loginForm').addEventListener('submit', function (event) {
-  event.preventDefault(); 
+document
+  .getElementById("loginForm")
+  .addEventListener("submit", function (event) {
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
 
-  var email = document.getElementById('email').value;
-  var password = document.getElementById('password').value;
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    var passwordRegex =
+      /^(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\/\-])(?=.*[A-Z])(?=.*\d.*\d).{8,}$/;
 
-  var passwordRegex = /^(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\/\-])(?=.*[A-Z])(?=.*\d.*\d).{8,}$/;
+    var isValidEmail = emailRegex.test(email);
+    var isValidPassword = passwordRegex.test(password);
 
-  var isValidEmail = emailRegex.test(email);
-  var isValidPassword = passwordRegex.test(password);
+    if (isValidEmail && isValidPassword) {
+      alert("Login exitoso");
+      location.reload();
 
-  if (isValidEmail && isValidPassword) {
-    alert('Login exitoso');
-    console.log("Email: " + email, "Password: " + password);
-    return;
-  } else {
-    var errorMessage = '';
+      return;
+    } else {
+      var errorMessage = "";
+      console.log("Email: " + email, "Password: " + password);
+      if (!isValidEmail) {
+        errorMessage += "El formato del correo electrónico no es válido. ";
+      }
 
-    if (!isValidEmail) {
-      errorMessage += 'El formato del correo electrónico no es válido. ';
+      if (!isValidPassword) {
+        errorMessage += "La contraseña no cumple con los requisitos mínimos.";
+      }
+
+      alert("Credenciales incorrectas. Inténtelo de nuevo.\n" + errorMessage);
     }
-
-    if (!isValidPassword) {
-      errorMessage += 'La contraseña no cumple con los requisitos mínimos.';
-    }
-
-    alert('Credenciales incorrectas. Inténtelo de nuevo.\n' + errorMessage);
-  }
-});
+  });
