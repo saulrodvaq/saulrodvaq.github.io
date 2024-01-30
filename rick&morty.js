ef85a6e6ad5ce229aa6b67ffc30f2c7d29aa406f
tae maro <saulrodvaq@gmail.com>
2024-01-30 10:52:13 -0600

    h

---
+++ rick&morty.js
@@ -0,0 +1,12 @@
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("https://rickandmortyapi.com/api/character")
+    .then((response) => response.json())
+    .then((data) => {
+      const characters = data.results;
+      characters.forEach((character) => {
+        console.log(character.name);
+      });
+    });
+});
