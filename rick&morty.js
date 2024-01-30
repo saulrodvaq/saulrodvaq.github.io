3d98f0d17c6fefbe127b2ed4cebd914e1d4473ce
tae maro <saulrodvaq@gmail.com>
2024-01-30 13:40:33 -0600

    asdd

--- rick&morty.js
+++ rick&morty.js
@@ -1,10 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
-  fetch("https://rickandmortyapi.com/api/character")
-    .then((response) => response.json())
-    .then((data) => {
-      const characters = data.results;
-      characters.forEach((character) => {
-        console.log(character.name);
-      });
-    });
+  const characterList = document.getElementById("character-list");
+  const characterTemplate = document.getElementById("character-template");
+  let lastCharacterId = 0;
+
+  async function fetchCharacterById(id) {
+    const response = await fetch(
+      `https://rickandmortyapi.com/api/character/${id}`
+    );
+    return response.json();
+  }
+
+  document.getElementById("spawn").addEventListener("click", async () => {
+    lastCharacterId++;
+
+    try {
+      const character = await fetchCharacterById(lastCharacterId);
+      const clone = characterTemplate.content.cloneNode(true);
+      clone.querySelector("[data-id='character-name']").textContent =
+        character.name;
+      clone.querySelector("[data-id='character-image']").src = character.image;
+      const listItem = document.createElement("li");
+      listItem.appendChild(clone);
+      characterList.appendChild(listItem);
+    } catch (error) {
+      alert("No hay más personajes disponibles.");
+      lastCharacterId--;
+    }
+  });
+
+  document.getElementById("clear").addEventListener("click", () => {
+    var itemList = document.getElementById("character-list");
+    itemList.replaceChildren();
+    lastCharacterId = 0;
+  });
 });
